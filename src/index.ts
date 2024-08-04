45684aeda5acaf1b417553bf8651b210f389bbd4
GoblinBackwards <22856555+GoblinBackwards@users.noreply.github.com>
2024-08-04 17:36:31 +0100

    Add sqlite based auth

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import express, { Express, Request, Response } from 'express'
 import multer from 'multer'
+require('dotenv').config()
+import sqlite3 from 'sqlite3'
+const db = new sqlite3.Database('database.sqlite')
+
+db.serialize(() => {
+    db.run('CREATE TABLE IF NOT EXISTS auth (authkey TEXT)')
+    db.run('CREATE TABLE IF NOT EXISTS link (key TEXT, fullurl TEXT)')
+})
 
 const app = express()
 const port = 3000
@@ -10,16 +18,28 @@ type FormBody = {
     authkey: string
 }
 
+type DbRowAuth = {
+    authkey: string
+}
+
+type DbRowLink = {
+    key: string,
+    fullurl: string
+}
+
 app.use(express.static('public'))
 
-app.post('/', upload.none(), (req: Request, res: Response) => {
-    const validkeys = ['meow', 'moo']
-    const body: FormBody = req.body;
-    if (validkeys.includes(body.authkey)) {
-        res.status(200).send('new link').end();
-    } else {
-        res.status(403).end();
-    }
+app.post('/', upload.none(), async (req: Request, res: Response) => {
+    db.all('SELECT * FROM auth', (err, rows: DbRowAuth[]) => {
+        const form: FormBody = req.body;
+        const authenticated: boolean = rows.map(r => r.authkey).includes(form.authkey);
+
+        if (authenticated) {
+            res.status(200).send('new link').end();
+        } else {
+            res.status(403).end();
+        }
+    });
 })
 
 app.get('/link/', (req: Request, res: Response) => {
